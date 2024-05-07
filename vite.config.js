1fe596966459978371c5e337e55500c4177c75d6
pimphand <faisaldwiki69@gmail.com>
2024-05-07 18:05:40 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,10 @@ export default defineConfig({
                 'resources/assets/js/jquery.js',
                 'resources/assets/js/summernote.js',
                 'resources/js/app.js',
+                'resources/assets/libs/lucide/umd/lucide.min.js',
+                'resources/assets/libs/simplebar/simplebar.min.js',
+                'resources/assets/libs/flatpickr/flatpickr.min.js',
+                'resources/assets/libs/@frostui/tailwindcss/frostui.js'
             ],
             refresh: true,
         }),
